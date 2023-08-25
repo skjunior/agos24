47372cb21517fd06a53b87ccd3a7d7c413223eca
skjunior <142546585+skjunior@users.noreply.github.com>
2023-08-24 21:37:49 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+let nome = prompt("Qualo seu nome?");
+let anoNascimento = prompt("Qula o seu nascimento?");
+let anoAtual = prompt("ano atual;");
+console.log("nome:". nome);
+console.log("Idade:", (anoAtual - anoNascimento));
+console.log("É maior de idade?", ((anoAtual - anoNascimento)>=18));
+console.log("Idade em 2050", ((2050 - anoNascimento)));
